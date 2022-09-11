a3e1f200423df2a0d7589f9969aad907d67dbea2
DesmondNzubechi <nzubestdesmond@gmail.com>
2022-09-11 05:17:46 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+AOS.init();
+
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+};
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
+
+let close = document.queryselector('.close');
+let open = document.queryselector('.open');
+let ul = document.queryselector('.ul');
+
+
+//Display Nav Links
+open.addEventListener('click', () => {
+  ul.style.right = '0';
+  open.style.display = 'none';
+  close.style.display = 'block';
+});
+
+
+//Hide Nav Links
+
+close.addEventListener('click', () => {
+  ul.style.right = '-500px';
+  open.style.display = 'block';
+  close.style.display = 'none';
+});
